3b1ccc982e6b8d55df4b6ab9dbd1e2ad04a2cf86
Guilherme Augusto <guibiston@gmail.com>
2025-10-08 14:27:36 -0300

    modificações

---
+++ categorys/addcategory.test.js
@@ -0,0 +1,16 @@
+const { spec } = require('pactum');
+
+it('Deve adicionar uma nova categoria no site ebac', () => {
+   awaitspec()
+        .post('http://lojaebac.ebaconline.art.br/graphql')
+        .withGraphQLQuery(`
+mutation AddCategory($name: String, $photo: String) {
+  addCategory(name: $name, photo: $photo) {
+    name
+    photo
+  }
+}
+  `)
+        .expectStatus(200)
+
+});
